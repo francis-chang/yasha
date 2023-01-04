8fb6764c51167e660758a68c54bc830dea434164
francis <francis.c@zohomail.com>
2023-01-03 20:56:22 -0800

    wee

--- api/src/routes/seed_db/index.ts
+++ api/src/routes/seed_db/index.ts
@@ -22,18 +22,23 @@ const seedDatabaseRouter = express.Router()
 
 seedDatabaseRouter.get('/getteams', (req, res) => {
     statsqueue.add('seedGetTeams', null)
+    res.status(200).json({ msg: 'TASK SENT' })
 })
 seedDatabaseRouter.get('/getrefs', (req, res) => {
     statsqueue.add('seedGetReferees', null)
+    res.status(200).json({ msg: 'TASK SENT' })
 })
 seedDatabaseRouter.get('/getGames', (req, res) => {
     statsqueue.add('seedGetGames', null)
+    res.status(200).json({ msg: 'TASK SENT' })
 })
 seedDatabaseRouter.get('/getplayers', (req, res) => {
     statsqueue.add('seedGetPlayers', null)
+    res.status(200).json({ msg: 'TASK SENT' })
 })
 seedDatabaseRouter.get('/getboxscore', (req, res) => {
     statsqueue.add('seedGetBoxScore', null)
+    res.status(200).json({ msg: 'TASK SENT' })
 })
 
 export default seedDatabaseRouter
