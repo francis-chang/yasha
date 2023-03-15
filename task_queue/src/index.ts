957e1d96a634416ebc2f7a9c0b524212b3ade077
francis <francis.c@zohomail.com>
2023-03-15 13:10:14 -0700

    wee

--- task_queue/src/index.ts
+++ task_queue/src/index.ts
@@ -3,6 +3,19 @@ import IORedis from 'ioredis'
 import jobProcessor from './tasks'
 import statsqueue from './utils/bullmqProducer'
 
+const addRepeatedJob = async (job_name: string, is_every: boolean, pattern: string | null, duration: number | null) => {
+    if (is_every && duration) {
+        await statsqueue.add(job_name, null, {
+            repeat: { every: duration },
+            removeOnComplete: {
+                age: 60,
+            },
+        })
+    } else if (!is_every && pattern) {
+        await statsqueue.add(job_name, null, { repeat: { pattern }, removeOnComplete: { age: 60 } })
+    }
+}
+
 if (process.env.REDIS_BULLMQ_URL) {
     const connection = new IORedis(process.env.REDIS_BULLMQ_URL, {
         maxRetriesPerRequest: null,
@@ -10,13 +23,6 @@ if (process.env.REDIS_BULLMQ_URL) {
 
     const worker = new Worker('myqueue', jobProcessor, {
         connection,
-        //@ts-ignore
-        removeOnComplete: {
-            age: 1200,
-        },
-        removeOnFail: {
-            age: 24 * 3600,
-        },
     })
 
     worker.on('completed', (job) => {
@@ -35,20 +41,21 @@ if (process.env.REDIS_BULLMQ_URL) {
         jobs.forEach(async (job) => {
             await statsqueue.removeRepeatableByKey(job.key)
         })
-        await statsqueue.add('loadDraftListForMock', null, { repeat: { every: 600000 } })
-        await statsqueue.add('updateInjuries', null, { repeat: { every: 600000 } })
-        await statsqueue.add('loadSingleDayStatlines', null, { repeat: { every: 10000 } })
-        await statsqueue.add('updateStandings', null, { repeat: { every: 600000 } })
-        await statsqueue.add('updateBoxScoreMinute', null, { repeat: { every: 100000 } })
-        await statsqueue.add('updateBoxScoreThreeSeconds', null, { repeat: { every: 5000 } })
-        await statsqueue.add('loadFourDayScores', null, { repeat: { every: 5000 } })
-        await statsqueue.add('loadTStatlines', null, { repeat: { every: 5000 } })
-        await statsqueue.add('loadGameInfo', null, { repeat: { every: 10000 } })
-        await statsqueue.add('loadTopAverages', null, { repeat: { pattern: '*/15 * * * *' } })
+        await addRepeatedJob('loadDraftListForMock', true, null, 600000)
 
-        await statsqueue.add('loadTopStatlines', null, { repeat: { pattern: '*/30 * * * *' } })
-        await statsqueue.add('loadComputerPickDraftList', null, { repeat: { pattern: '0 0 * * *' } })
-        await statsqueue.add('loadDraftList', null, { repeat: { every: 10000 } })
+        await addRepeatedJob('updateInjuries', true, null, 600000)
+        await addRepeatedJob('loadSingleDayStatlines', true, null, 10000)
+        await addRepeatedJob('updateStandings', true, null, 600000)
+        await addRepeatedJob('updateBoxScoreMinute', true, null, 100000)
+        await addRepeatedJob('updateBoxScoreThreeSeconds', true, null, 5000)
+        await addRepeatedJob('loadFourDayScores', true, null, 5000)
+        await addRepeatedJob('loadTStatlines', true, null, 5000)
+        await addRepeatedJob('loadGameInfo', true, null, 10000)
+        await addRepeatedJob('loadDraftList', true, null, 10000)
+
+        await addRepeatedJob('loadTopAverages', false, '*/15 * * * *', null)
+        await addRepeatedJob('loadTopStatlines', false, '*/30 * * * *', null)
+        await addRepeatedJob('loadComputerPickDraftList', false, '0 0 * * *', null)
     })()
 } else {
     console.log('env var TQ_STATS_URL not found, please check bull service in docker-compose.yml')
